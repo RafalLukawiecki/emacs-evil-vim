f2d5c717c2e46dd3bc85efb69240fe9d0fa7391c
Rafal Lukawiecki <rafal@projectbotticelli.com>
2020-03-20 13:23:41 +0000

    Minor tidy up of setup-freebsd-emacs.sh

--- setup-freebsd-emacs.sh
+++ setup-freebsd-emacs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -x
+# set -x
 # RLL 20FEB20 Compiles and installs ncurses and emacs-nox on FreeBSD without termcap support,
 # so that it works with 24-bit color in terminal sessions
 
@@ -13,10 +13,14 @@ else
     fi
 
     yes | pkg remove emacs-nox ncurses
-    sed -E -i '' -e "s/enable-termcap/disable-termcap/g" /usr/ports/devel/ncurses/Makefile
-    make -DBATCH -C /usr/ports/devel/ncurses install clean
+    # The following perhaps temporarily only needed to resolve version mismatches when building emacs
+    yes | pkg install p5-Locale-gettext p5-Locale-libintl p5-Text-Unidecode p5-Unicode-EastAsianWidth
 
-    cat > /tmp/xterm-24bit.terminfo << EOF
+    sed -E -i '' -e "s/enable-termcap/disable-termcap/g" /usr/ports/devel/ncurses/Makefile
+
+    if make -DBATCH -C /usr/ports/devel/ncurses install clean; then
+
+        cat > /tmp/xterm-24bit.terminfo << EOF
 # Use colon separators.
 xterm-24bit|xterm with 24-bit direct color mode,
    use=xterm-256color,
@@ -29,8 +33,18 @@ xterm-24bits|xterm with 24-bit direct color mode,
    setf24=\E[38;2;%p1%{65536}%/%d;%p1%{256}%/%{255}%&%d;%p1%{255}%&%dm,
 EOF
 
-    tic -x -s /tmp/xterm-24bit.terminfo
-    make -DBATCH -C /usr/ports/editors/emacs FLAVOR=nox install clean
+        tic -x -s /tmp/xterm-24bit.terminfo
+        rm /tmp/xterm-24bit.terminfo
 
-    printf "Restart emacs daemon if it is running.\n"
+        if make -DBATCH -C /usr/ports/editors/emacs FLAVOR=nox install clean; then
+            printf "Restart emacs daemon if it is running.\n"
+        else
+            printf "Could not compile and install emacs\n"
+            exit 1
+        fi
+    else
+        printf "Could not compile and install ncurses\n"
+        exit 1
+    fi
+
 fi
